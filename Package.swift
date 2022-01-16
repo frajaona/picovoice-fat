2f43617286d5330ba099b16073cbb15ce0161f70
Fred RAJAONA <fred@kannelle.io>
2022-01-16 10:47:08 +0100

    Use tagged ios-voice-processor version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ledoufre/ios-voice-processor.git", .branch("feat-spm-support")),
+        .package(url: "https://github.com/ledoufre/ios-voice-processor.git", from: "1.0.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
