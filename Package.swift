bb03002d2186d955406dba81cc00ee467ce0576c
Fred Rajaona <frajaona@gmail.com>
2024-12-01 16:25:45 +0100

    Use only SPM dependencies

--- Package.swift
+++ Package.swift
@@ -14,9 +14,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Picovoice/ios-voice-processor", from: "1.2.0"),
         .package(url: "https://github.com/Picovoice/porcupine", branch: "master"),
+        .package(url: "https://github.com/Picovoice/rhino", branch: "master"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,14 +26,8 @@ let package = Package(
             dependencies: [
                 .product(name: "ios_voice_processor", package: "ios-voice-processor"),
                 .product(name: "Porcupine", package: "porcupine"),
-                "PvRhino"
+                .product(name: "Rhino", package: "rhino"),
             ]),
-        /*.binaryTarget(
-            name: "PvPorcupine",
-            path: "lib/PvPorcupine.xcframework"),*/
-        .binaryTarget(
-            name: "PvRhino",
-            path: "lib/PvRhino.xcframework"),
         .testTarget(
             name: "picovoicefatTests",
             dependencies: ["picovoicefat"]),
